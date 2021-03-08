fed55e1d5f1e1bae7e4352c63c2ddd0e43d7d07a
chinmayingle91 <74113444+chinmayingle91@users.noreply.github.com>
2021-03-08 10:18:05 +0530

    Update logic.js
    update logic file for comments

--- logic.js
+++ logic.js
@@ -1,7 +1,10 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//  Declare array of for appendig no. of elements
+let = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+//Get element  
 let getElement = document.getElementById('item');
 
+// Add elements and added class to every element
 arr.forEach((nodes, i) => {
     let newElement = document.createElement('div');
     getElement.appendChild(newElement);
@@ -10,17 +13,16 @@ arr.forEach((nodes, i) => {
     newElement.innerHTML = nodes;
 })
 
+// Shuffles element
 function shuffleElements() {
-   // let items = document.getElementById('item');
     for (let i = arr.length - 1; i > 0; i--) {
-       // console.log(getElement.children[Math.floor(Math.random() * (i + 1))])
-       getElement.appendChild(getElement.children[Math.floor(Math.random() * (i + 1))])
+        getElement.appendChild(getElement.children[Math.floor(Math.random() * (i + 1))])
     }
 }
-function sortElements() {
-    //let list = document.getElementById('item');
 
-    [...getElement.children]
+//sort the elements
+function sortElements() {
+ [...getElement.children]
         .sort((a, b) => Number(a.innerText) > Number(b.innerText) ? 1 : -1)
         .forEach(node => getElement.appendChild(node));
 }
